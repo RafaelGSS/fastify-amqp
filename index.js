4f65ff0d5a5fb6960667bafbf60d8cd9e972d5d6
Rafael Gonzaga <rafael.gonzaga@climatempo.com.br>
2019-06-21 22:15:52 -0300

    feat: Initial version

---
+++ index.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+const amqpClient = require('amqplib')
+
+function fastifyAmqp (fastify, opts, next) {
+  const host = opts.host
+
+  if (!host) {
+    next(new Error('`host` parameter is mandatory'))
+    return
+  }
+  const port = opts.port || 5672
+
+  amqpClient.connect(`amqp://${host}:${port}`, function (err, connection) {
+    if (err) {
+      next(err)
+      return
+    }
+    fastify.addHook('onClose', () => connection.close())
+    fastify.decorate('amqpConn', connection)
+    
+    connection.createChannel(function (err1, channel) {
+      if (err1) {
+        next(err1)
+        return
+      }
+
+      fastify.decorate('amqpChannel', channel)
+    })
+  })
+
+}
+
+module.exports = fp(fastifyAmqp, {
+  fastify: '>=1.0.0',
+  name: 'fastify-amqp'
+})
